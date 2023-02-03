30cd57afc0a12eb80ed43b0be272834ce2991dec
hossam hassan <hossamhassan@hossams-MacBook-Air.local>
2023-02-03 22:01:24 +0200

    setup relations between blog and users

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,8 @@
 class Blog < ApplicationRecord
+
+  #validations
   validates_presence_of :title, :body
+
+  #relations
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2023_02_03_194942) do
     t.string "name"
     t.string "username"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
